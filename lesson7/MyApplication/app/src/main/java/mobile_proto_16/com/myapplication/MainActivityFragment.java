98e58f8d2ec7767e308c87ee6e2b590745a7403b
David Abrahams <davidabrahams@sbcglobal.net>
2016-09-26 12:04:15 -0400

    updating boilerplate

--- lesson7/MyApplication/app/src/main/java/mobile_proto_16/com/myapplication/MainActivityFragment.java
+++ lesson7/MyApplication/app/src/main/java/mobile_proto_16/com/myapplication/MainActivityFragment.java
@@ -2,14 +2,34 @@ package mobile_proto_16.com.myapplication;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+
+import butterknife.ButterKnife;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private Button button;
+    private EditText input;
+    private TextView view;
+
+    private Response.Listerer = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            // Display the first 500 characters of the response string.
+            mTextView.setText("Response is: "+ response.substring(0,500));
+        }
+    }
 
     public MainActivityFragment() {
     }
@@ -17,6 +37,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
+
+    private String buildSearchURL(String companyTicker) {
+        // YOUR CODE HERE
+        return "http://finance.google.com/finance/info?client=iq&q=aapl";
+    }
+
+    private String extractPriceFromJSON(JSONArray array) {
+        // YOUR CODE HERE
+        return "";
+    }
+
 }
